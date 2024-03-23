92f852e839d511b7ed82966ca6886c295fe2f53d
cris4crisland <160337227+cris4crisland@users.noreply.github.com>
2024-03-23 11:28:22 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const randomIndexFunction = length => {
+    return Math.floor(Math.random() * length);
+  };
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const randomExcuse = `${who[randomIndexFunction(who.length)] +
+    " " +
+    action[randomIndexFunction(action.length)] +
+    " " +
+    what[randomIndexFunction(what.length)] +
+    " " +
+    when[randomIndexFunction(when.length)]}`;
+
+  document.getElementById("excuse").innerHTML = randomExcuse;
+
+  console.log("Hello Ringo from the console!");
 };
